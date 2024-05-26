e4a283e17e64f2110f22dce3e25dbda0f36f6c08
zyhwitness <zyhwitness23@gmail.com>
2024-05-26 15:56:17 +0800

    zyhwitness, feat: prepare

--- QuickStarter/src/main/java/com/security/SecurityApplication.java
+++ QuickStarter/src/main/java/com/security/SecurityApplication.java
@@ -2,12 +2,14 @@ package com.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SecurityApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecurityApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(SecurityApplication.class, args);
+        System.out.println("1111");
     }
 
 }
---
+++ TokenDemo/src/main/java/com/token/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.token.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: TODO
+ * @Author: iWitness
+ * @Date: 2024/5/24 15:51
+ * @Version 1.0
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+}
