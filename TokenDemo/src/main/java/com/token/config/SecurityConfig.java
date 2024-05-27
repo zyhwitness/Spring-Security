3667ec66e5ae8e98bdc35e221bcfee1803590824
zyhwitness <zyhwitness23@gmail.com>
2024-05-27 15:20:04 +0800

    zyhwitness, feat: BCryptPasswordEncoder

---
+++ TokenDemo/src/main/java/com/token/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.token.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @Description: TODO
+ * @Author: iWitness
+ * @Date: 2024/5/27 10:12
+ * @Version 1.0
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //创建BCryptPasswordEncoder注入容器
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
--- TokenDemo/src/test/java/com/token/MapperTest.java
+++ TokenDemo/src/test/java/com/token/MapperTest.java
@@ -5,6 +5,7 @@ import com.token.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -20,10 +21,34 @@ public class MapperTest {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Test
     public void testUserMapper(){
         List<User> users = userMapper.selectList(null);
         System.out.println(users);
     }
+
+    @Test
+    public void testBCryptPasswordEncoder(){
+
+        boolean matches = passwordEncoder.matches("admin",
+                "$2a$10$6il0bGhzWWalUueYvpRX9ODGQ9blP0RJ3r/jVIjKzuwteUz/HiAuW");
+        System.out.println(matches);
+
+        String encode = passwordEncoder.encode("1234");
+        String encode1 = passwordEncoder.encode("12345678");
+
+        //$2a$10$LmoRnV86dTB2JWOKCLAUj.PorMSTePiznuBUrVZ97ef6I6QUz8wsG
+        //$2a$10$gNwW1hZ4UFJ59NdW3/FKCObtJ1YbZgY2mYmo4/aHkwbUDEmJYUolS
+
+        //$2a$10$uZnWqvEVtzQNM540FekGseFAkHScfZX0PR/zxfwfbcu8SuXDu15pO
+
+        //System.out.println(encode);
+        System.out.println(encode1);
+
+    }
+
 
 }
